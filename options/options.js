061835e0234dff3341fb6a568c6aee40d35abbf7
GamerBoi801 <geeseplayer@gmail.com>
2025-03-11 22:21:43 +0500

    first time testing

--- options/options.js
+++ options/options.js
@@ -1,11 +1,11 @@
 //options.js = user can change the settings of the extension
 
-//loads the current settings
-chrome.storage.local.get({retainStacks: false }, (data) => {
+// Load saved options
+chrome.storage.local.get({ retainStacks: false }, (data) => {
     document.getElementById("retainStacks").checked = data.retainStacks;
-});
-
-//saves the settings when the user changes them
-document.getElementById("retainStacks").addEventListener("change", (event) => {
-    chrome.storage.local.set({retainStacks: event.target.checked});
-});
+  });
+  
+  // Save options when changed
+  document.getElementById("retainStacks").addEventListener("change", (e) => {
+    chrome.storage.local.set({ retainStacks: e.target.checked });
+  });
